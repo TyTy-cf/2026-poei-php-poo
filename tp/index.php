52974827173b0ffd7887c143ed7c4d659ab67bd3
BriceVaillant <naw.brice@gmail.com>
2026-06-29 15:28:28 +0200

    all done

--- tp/index.php
+++ tp/index.php
@@ -1,22 +1,53 @@
 <?php
-include_once "./Utility/utility.php";
+include_once "../src/Utility/utility.php";
 include_once "./Entity/Brands.php";
 include_once "./Entity/Categories.php";
 include_once "./Entity/Listings.php";
 include_once "./Entity/Models.php";
 include_once "./Entity/Sellers.php";
 
-$brand = new Brands();
-$category = new Categories();
-$listing = new Listings();
-$model = new Models();
-$seller = new Sellers();
+$swift = new Brands();
+$swift->setId(1);
+$swift->setLabel("Suzuki Swift");
+$swift->setDescription("c'est une bonne voiture.");
 
-dump($brand);
-dump($category);
-dump($listing);
+
+$citadine = new Categories();
+$citadine->setId(3);
+$citadine->setLabel("Citadine");
+$citadine->setDescription("ceci est une citadine");
+
+$model = new Models();
+$model->setId(1);
+$model->setLabel("Swift 1.2");
+$model->setBrand($swift);
+$model->setCategory($citadine);
+
+$brice = new Sellers();
+$brice->setId(1);
+$brice->setFirstname("Brice");
+$brice->setLastname("véyan");
+$brice->setPhoneNumber("06 12 34 56 78");
+$brice->setLocation("Pardiey");
+$brice->setEmail("brice@véyan.com");
+
+$listing = new Listings();
+$listing->setId(1);
+$listing->setSeller($brice);
+$listing->setModel($model);
+$listing->setTitle("Swift 1.2");
+$listing->setDescription("Swift 1.2, 2015, 120000 km, 8000€");
+$listing->setProduceYear("2015");
+$listing->setMileage(120000);
+$listing->setPrice(8000);
+$listing->setPublishAt(date("Y-m-d H:i:s"));
+
+dump($swift);
+dump($citadine);
 dump($model);
 dump($seller);
+dump($listing);
+
 ?>
 
 <!DOCTYPE html>
