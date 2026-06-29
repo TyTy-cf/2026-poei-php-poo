18c0d0bdf4dcc00bb8c751e0bdde1bf7174d09bd
BriceVaillant <naw.brice@gmail.com>
2026-06-29 14:46:14 +0200

    blbabla

--- tp/Entity/Brands.php
+++ tp/Entity/Brands.php
@@ -1,14 +1,41 @@
 <?php
 
-namespace Entity;
 
 class Brands
 {
 
-    private int $id;
+  private int $id;
+  private string $label;
+  private string $description;
 
-    private string $label;
 
-    private string $description;
+  public function getId(): int
+  {
+    return $this->id;
+  }
 
+  public function setId(int $id): void
+  {
+    $this->id = $id;
+  }
+
+  public function getLabel(): string
+  {
+    return $this->label;
+  }
+
+  public function setLabel(string $label): void
+  {
+    $this->label = $label;
+  }
+
+  public function getDescription(): ?string
+  {
+    return $this->description;
+  }
+
+  public function setDescription(?string $description): void
+  {
+    $this->description = $description;
+  }
 }
