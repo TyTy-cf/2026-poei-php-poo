abfa6bb668035a1ade1a702ddc235f7148c69b1d
Pierre Allimant <drosalys@ordi-10.cyberfull.local>
2026-06-30 16:28:20 +0200

    aled

--- tp/Entity/PokemonRepository.php
+++ tp/Entity/PokemonRepository.php
@@ -4,18 +4,27 @@ include_once "../src/Utility/utility.php";
 
 class PokemonRepository
 {
-function __construct(){
-    $pdo = new PDO(
+
+private PDO $pdo;
+
+public function __construct(){
+    $this->pdo = new PDO(
         'mysql:host=mariadb;dbname=db_pokemons;port=3306',
         'root',
         'root'
     );
 
-
+}
+public function fetchAll(): array{
     $sql = "SELECT * FROM pokemon";
     $stmt = $pdo->prepare($sql);
     $stmt->execute();
     $pokemons = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
+    return $pokemons;
 }
+
+public function fetchByID() : array{
+    $sql = "SELECT * FROM pokemon WHERE "
+}
+
 }
