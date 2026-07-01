f2fd47371ac6c655e33aa2b6f63a219a9599680b
Kevin Tourret <kevin@drosalys.fr>
2026-07-01 11:42:39 +0200

    example chess

--- src/Templates/chess.php
+++ src/Templates/chess.php
@@ -10,33 +10,33 @@ include_once "../Entity/Chess/Bishop.php";
 include_once "../Utility/utility.php";
 include_once "header.php";
 
-$rook = new Rook('black');
-$knight = new Knight('black');
-$queen = new Queen('black');
-$king = new King('black');
-$pawn = new Pawn('black');
-$bishop = new Bishop('black');
+//$rook = new Rook('black');
+//$knight = new Knight('black');
+//$queen = new Queen('black');
+//$king = new King('black');
+//$pawn = new Pawn('black');
+//$bishop = new Bishop('black');
 
-$rook->move();
-echo "<br>";
-$knight->move();
-echo "<br>";
-$queen->move();
-echo "<br>";
-$king->move();
-echo "<br>";
-$pawn->move();
-echo "<br>";
-$bishop->move();
+//$rook->move();
+//echo "<br>";
+//$knight->move();
+//echo "<br>";
+//$queen->move();
+//echo "<br>";
+//$king->move();
+//echo "<br>";
+//$pawn->move();
+//echo "<br>";
+//$bishop->move();
 
-//dump(new Rook('black'));
-//dump(new Rook('black'));
-//dump(new Rook('white'));
-//dump(new Rook('white'));
-//dump(new Knight('black'));
-//dump(new Knight('black'));
-//dump(new Knight('white'));
-//dump(new Knight('white'));
+dump(new Rook('black'));
+dump(new Rook('black'));
+dump(new Rook('white'));
+dump(new Rook('white'));
+dump(new Knight('black'));
+dump(new Knight('black'));
+dump(new Knight('white'));
+dump(new Knight('white'));
 
 
 ?>
