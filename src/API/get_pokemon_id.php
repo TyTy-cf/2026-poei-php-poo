60cf1318091b5df4a777b328f7c03d493cd67a46
BarbierQ <barbierquentin@hotmail.com>
2026-07-02 16:14:44 +0200

    marche pas

--- src/API/get_pokemon_id.php
+++ src/API/get_pokemon_id.php
@@ -1,12 +1,25 @@
 <?php
 
+include __DIR__ . "/../include.php";
+
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+
+if ($httpMethod !== "GET") {
+    header('Content-Type: application/json; charset=utf-8', false, 400);
+    echo 'Bad request';
+    return;
+}
+
 // GET http://localhost:8000/api/pokemon/5 => fetchById
 // GET http://localhost:8000/src/API/get_pokemon.php?id=5 => fetchById
+
+header('Content-Type: application/json; charset=utf-8', false, 200);
 
 if (isset($_GET['id'])) {
     // Via PokemonRepository faire le fetchById
     $repository = new PokemonRepository();
     $pokemon = $repository->fetchById($_GET['id']);
+    echo json_encode($pokemon);
 }
 
 
--- src/API/post_pokemon.php
+++ src/API/post_pokemon.php
@@ -1,10 +1,33 @@
 <?php
 
+include __DIR__ . "/../include.php";
+
+
+function getBody(): string{
+    return json_encode(file_get_contents("php://input"));
+}
+
 // POST http://localhost:8000/api/pokemon => create
-// POST http://localhost:8000/src/API/get_pokemon.php => create
+// POST http://localhost:8000/src/API/post_pokemon.php => create
+
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+
+if ($httpMethod !== "POST") {
+    header('Content-Type: application/json; charset=utf-8', false, 400);
+    echo 'Bad request';
+    return;
+}
+
 
 if (isset($_POST['name'])) {
     // Ajout en BDD
+    $repository = new PokemonRepository();
+    // j'ai besoin d'un array
+    $pokemon = $repository->create(getBody());
+
+    header('Content-Type: application/json; charset=utf-8', false, 200);
+
+    echo json_encode($pokemon);
 }
 
 // Renvoyer sous forme de JSON l'objet créé avec son ID
